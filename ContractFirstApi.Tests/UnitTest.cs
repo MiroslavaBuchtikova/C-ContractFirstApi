7b83b7ebd640de855403eb5ed24e02d9573a6c51
karasova <karasova@KARASOVA>
2022-01-07 12:42:47 +0100

    Setup

---
+++ ContractFirstApi.Tests/UnitTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace ContractFirstApi.Tests
+{
+    [TestClass]
+    public class UnitTest : TestBase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {         
+            //var user = CreateClient().ListUserAsync(10).GetAwaiter().GetResult();
+        }
+     
+    }
+}
